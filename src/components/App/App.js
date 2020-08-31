9fa93a1a59907ce9ad7727d8c375207d16005109
Vladislav <zippo585@gmail.com>
2020-08-31 18:30:14 +0500

    Error with Filter Container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import axios from 'axios';
-import { Card, Container } from 'semantic-ui-react';
+import axios from "axios";
+import { Card, Container } from "semantic-ui-react";
 
 //scss
 import "css/index.css";
 import "./App.css";
 
 //components
-import {Header, Filter, Recipes} from "components";
-import {Recipe} from "containers";
+import { Header } from "components";
+import { Recipe, Filter } from "containers";
 
 class App extends Component {
     constructor(props) {
@@ -18,12 +18,16 @@ class App extends Component {
     componentWillMount() {
         const { setRecipes } = this.props;
         console.log("checkmount");
-        axios.get('/recipes.json').then(({ data }) => {
-            console.log(data,'data');
-            setRecipes(data);
-           
-        }).catch((e) => {throw new Error(e + 'Получение данных из json файла')});
-      }
+        axios
+            .get("/recipes.json")
+            .then(({ data }) => {
+                console.log(data, "data");
+                setRecipes(data);
+            })
+            .catch((e) => {
+                throw new Error(e + "Получение данных из json файла");
+            });
+    }
 
     render() {
         const { recipes, isReady } = this.props;
@@ -34,8 +38,10 @@ class App extends Component {
                 {/* <Recipes /> */}
                 <Card.Group itemsPerRow={4}>
                     {!isReady
-                        ? 'Загрузка...'
-                        : recipes.map((book, i) => <Recipe key={i} {...book} />)}
+                        ? "Загрузка..."
+                        : recipes.map((book, i) => (
+                              <Recipe key={i} {...book} />
+                          ))}
                 </Card.Group>
             </Container>
         );
