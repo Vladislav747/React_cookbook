450d83ffb135ca1df98b14eba373edc2252aeb9a
Vladislav_747 <zippo585@gmail.com>
2020-09-02 13:54:50 +0500

    Created Recipe test

---
+++ public/recipesArray.js
@@ -0,0 +1,101 @@
+let arrayJs = [
+    {
+        id: 0,
+        title: "Детское яблочное пюре",
+        author: "Иванова Ольга Сергеевна",
+        image:
+            "https://yummybook.ru/upload/images/recipe/item/9738/image_large_39865f3c29eab214097779178_97779178.jpg",
+        ingredients: ["Яблоко 2-3 шт", "Вода 250 мл"],
+        likes: 710,
+    },
+    {
+        id: 1,
+        title: "Домашние молочные сосиски",
+        author: "Джордж Оруэлл",
+        image:
+            "https://yummybook.ru/upload/images/recipe/item/9736/image_large_56405f3b9a6c4920597741738_97741738.jpg",
+        ingredients: [
+            "Куриное филе - 2 шт",
+            "Молоко - 250 мл",
+            "Свекла (вареная) - 30 г",
+            "Растительное масло - 20 мл",
+            "Соль",
+            "смесь перцев - по вкусу",
+        ],
+        likes: 415,
+    },
+    {
+        id: 2,
+        title: "Детское морковное пюре",
+        author: "Донато Карризи",
+        image:
+            "https://yummybook.ru/upload/images/recipe/item/9737/image_large_45715f3c224a77ed297776688_97776688.jpg",
+        ingredients: ["Морковка 250 г", "Вода 200 мл"],
+        likes: 464,
+    },
+    {
+        id: 3,
+        title: "Гуляш с чипетке",
+        author: "Михаил Зыгарь",
+        image:
+            "https://yummybook.ru/upload/images/recipe/item/3821/image_large_69225f2d6a376ca06_96811855.jpg",
+        ingredients: ["1 болгарский перец", "4 картофелины", "Свинина 500 г"],
+        likes: 741,
+    },
+    {
+        id: 4,
+        title: "Гнёзда из кабачков",
+        author: "Татьяна Устинова",
+        image:
+            "https://yummybook.ru/upload/images/recipe/item/9703/image_large_88355f2d664d42cf7_96810865.jpg",
+        ingredients: ["300 г. кабачков", "30 г. твёрдого сыра", "1 яйцо"],
+        likes: 317,
+    },
+
+    {
+        id: 14,
+        title: "Салат из овощей с пикантной заправкой «Шехерезада»",
+        author: "Александра Маринина",
+        image:
+            "https://yummybook.ru/upload/images/recipe/item/9730/image_large_31245f3a8d58ca6eb97673010_97673011.jpg",
+        ingredients: [
+            "Перец болгарский – 2 шт. (300 г)",
+            "Баклажан –1 шт. (150 г)",
+            "Лук 500 г",
+        ],
+        likes: 664,
+    },
+    {
+        id: 15,
+        title: "Суфле из кабачков",
+        author: "Гиллиан Флинн",
+        image:
+            "https://yummybook.ru/upload/images/recipe/item/9693/image_large_56865f1b08c2ce68c_95607257.jpg",
+        ingredients: [
+            "500 г. кабачка",
+            "3 яйца",
+            "50 г. сыра",
+            "соль",
+            "перец",
+        ],
+        likes: 476,
+    },
+    {
+        id: 17,
+        title: "Витаминный салат из капусты",
+        author: "Дмитрий Глуховский",
+        image:
+            "https://yummybook.ru/upload/images/recipe/item/9630/image_large_53215ef2101e5c61f92922490_92922490.jpg",
+        ingredients: [
+            "300 гр белокочанной капусты",
+            "1 морковь",
+            "1 репчатый лук (или салатный)",
+            "300 гр консервированного горошка",
+            "1 пучок укропа",
+            "соль по вкусу",
+        ],
+        likes: 837,
+    },
+];
+
+export default arrayJs;
--- src/tests/Recipe.test.js
+++ src/tests/Recipe.test.js
@@ -2,15 +2,17 @@
 import React from "react";
 import { create, update } from "react-test-renderer";
 
-import recipes from "../../public/recipes.json";
+import recipes from "../../public/recipesArray.js";
 import { addLike, deleteRecipe } from "../redux/actions/recipes.js";
 import Recipe from "../components/Recipe/Recipe";
 
-it("renders without crashing", () => {
-    var recipesData = JSON.parse(recipes);
+it("Component Recipe renders without crashing", () => {
     let tree = create(
         <Recipe
-            ingredients={recipesData}
+            ingredients={recipes[0].ingredients}
+            title={recipes[0].title}
+            author={recipes[0].author}
+            image={recipes[0].image}
             addLike={addLike}
             deleteRecipe={deleteRecipe}
         />
